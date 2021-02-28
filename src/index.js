aac97525ad8290e1da964dfe27eb62301e319ec5
cs-py <e.1@mail.ru>
2021-02-28 16:45:42 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0
+  return array.reduce((acc, el) => el<acc ? el : acc, Infinity)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0
+  return array.reduce((acc, el) => el>acc ? el : acc, -Infinity)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0
+  return array.reduce((acc, el) => acc + el, 0) / array.length
 }
